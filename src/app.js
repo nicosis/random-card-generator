50ed62bad8d835e758eab8f2af11b5ed2905c304
nicosis <nicosis@gmail.com>
2023-03-18 18:45:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,44 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = () => Math.floor(Math.random() * 13) + 1;
+  let randomSuit = () => Math.floor(Math.random() * 4) + 1;
+
+  let card = num => {
+    switch (num) {
+      case 1:
+        return "A";
+        break;
+      case 11:
+        return "J";
+        break;
+      case 12:
+        return "Q";
+        break;
+      case 13:
+        return "K";
+        break;
+      default:
+        return num.toString();
+        break;
+    }
+  };
+
+  let suit = num => {
+    switch (num) {
+      case 1:
+        return "Spade ♠";
+      case 2:
+        return "Club ♣";
+      case 3:
+        return "Heart ♥";
+      case 4:
+        return "Diamond ♦";
+    }
+  };
+
+  let resultCard = "";
+  resultCard = card(randomNumber()) + " of " + suit(randomSuit());
+
+  console.log(resultCard);
 };
