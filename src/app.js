bfff51a13011e52de2e8ce93508397f791f371e9
nicosis <nicosis@gmail.com>
2023-03-25 09:09:34 +0000

    ajuste del textContent para clases

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = cardGenerator;
+
 function cardGenerator() {
   let cardValues = [
     "A",
@@ -21,30 +23,37 @@ function cardGenerator() {
 
   let suitValues = ["♠", "♣", "♥", "♦"];
 
-  let card = () => cardValues[Math.floor(Math.random() * cardValues.length)];
-  let suit = () => suitValues[Math.floor(Math.random() * suitValues.length)];
+  let randomValue = () =>
+    cardValues[Math.floor(Math.random() * cardValues.length)];
+  let randomSuit = () =>
+    suitValues[Math.floor(Math.random() * suitValues.length)];
 
-  let cardNum = document.querySelector("#cardNum");
-  let cardSuit = document.querySelector("#cardSuitTop");
+  let value = randomValue();
+  let cornerValue = document.querySelectorAll(".card-value");
+  cornerValue.forEach(x => (x.textContent = value));
 
-  // porque primero no se llama al id con querySelector?
-  cardNum.textContent = card();
-  cardSuitTop.textContent = suit();
-  cardNumTop.textContent = cardNum.textContent;
-  cardSuitBot.textContent = cardSuitTop.textContent;
-  cardNumBot.textContent = cardNum.textContent;
+  let suit = randomSuit();
+  let cornerSuit = document.querySelectorAll(".card-suit");
+  cornerSuit.forEach(x => (x.textContent = suit));
 
-  if (cardSuitTop.textContent === "♥" || cardSuitTop.textContent === "♦") {
-    document.querySelector(".card").style.color = "red";
+  // condicionales para los ases y el color
+  if (value === "A") {
+    document.querySelector("#middle").textContent = suit;
   }
-
-  if (cardNum.textContent === "A") {
-    document.querySelector("#cardNum").textContent = cardSuitTop.textContent;
+  if (suit === "♥" || suit === "♦") {
+    document.querySelector(".card").style.color = "red";
+  } else {
+    document.querySelector(".card").style.color = "black";
   }
 }
-
-window.onload = cardGenerator;
 
 //boton
 let button = document.querySelector("#changeCardBtn");
 button.addEventListener("click", cardGenerator);
+
+// Metodo con ID's
+// cardNum.textContent = card();
+// cardSuitTop.textContent = suit();
+// cardNumTop.textContent = cardNum.textContent;
+// cardSuitBot.textContent = cardSuitTop.textContent;
+// cardNumBot.textContent = cardNum.textContent;
